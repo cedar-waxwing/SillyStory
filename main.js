5238e3a58bd13c7fbd2035e32e99c9f131df361b
Rachel Ehlers <rachel.ehlers1@gmail.com>
2021-02-11 11:40:07 -0800

    Debug JavaScript

--- main.js
+++ main.js
@@ -38,27 +38,27 @@ randomize.addEventListener('click', result);
 function result() {
 /* This is needed so we can create a new random story each time the button is pressed and the function is run. If we made changes directly to storyText, we'd only be able to generate a new story once.*/
 
-  var newStory = storyText
+  var newStory = storyText;
 
 /*Create three new variables called xItem, yItem, and zItem, and make them equal to the result of calling randomValueFromArray() on your three arrays (the result in each case will be a random item out of each array it is called on). For example you can call the function and get it to return one random string out of insertX by writing randomValueFromArray(insertX)*/
 
-  var xItem = randomValueFromArray(insertX)
-  var yItem = randomValueFromArray(insertY)
-  var zItem = randomValueFromArray(insertZ)
+  var xItem = randomValueFromArray(insertX);
+  var yItem = randomValueFromArray(insertY);
+  var zItem = randomValueFromArray(insertZ);
 
 /*Next we want to replace the three placeholders in the newStory string — :insertx:, :inserty:, and :insertz: — with the strings stored in xItem, yItem, and zItem. There is a particular string method that will help you here — in each case, make the call to the method equal to newStory, so each time it is called, newStory is made equal to itself, but with substitutions made. So each time the button is pressed, these placeholders are each replaced with a random silly string. As a further hint, the method in question only replaces the first instance of the substring it finds, so you might need to make one of the calls twice.
 */
 
-newStory = newStory.replace("insertx", "xItem")
-newStory = newStory.replace("inserty", "yItem")
-newStory = newStory.replace("insertz", "zItem")
+newStory = newStory.replace(":insertx:", "xItem");
+newStory = newStory.replace(":inserty:", "yItem");
+newStory = newStory.replace(":insertz:", "zItem");
 
 
 /*Inside the first if block, add another string replacement method call to replace the name 'Bob' found in the newStory string with the name variable. In this block we are saying "If a value has been entered into the customName text input, replace Bob in the story with that custom name."*/
 
   if(customName.value !== "Bob") {
     let name = customName.value;
-    newStory = newStory.replace("Bob", "name")
+    newStory = newStory.replace("Bob", name);
   }
 
   /*Inside the second if block, we are checking to see if the uk radio button has been selected. If so, we want to convert the weight and temperature values in the story from pounds and Fahrenheit into stones and centigrade. What you need to do is as follows:
@@ -71,11 +71,11 @@ newStory = newStory.replace("insertz", "zItem")
 
   /*Inside the line that defines the temperature variable, replace 94 with a calculation that converts 94 Fahrenheit into centigrade. Concatenate ' centigrade' onto the end of the result of the overall Math.round() call.*/
 
-    let temperature =  Math.round((94 − 32) × 5/9) + " centigrade";
+    let temperature =  Math.round((94 − 32) × (5/9)) + " centigrade";
 
   /*Just under the two variable definitions, add two more string replacement lines that replace '94 fahrenheit' with the contents of the temperature variable, and '300 pounds' with the contents of the weight variable.*/
-    NewStory = newStory.replace("94 farenheit", "tempurature")
-    NewStory = newStory.replace("300 pounds", "weight")
+    newStory = newStory.replace("94 farenheit", tempurature);
+    newStory = newStory.replace("300 pounds", weight);
 
   }
 
